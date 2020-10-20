78b1298215cc60a0626cf79085e3b65fd2c2ec9b
= <willpat15@gmail.com>
2020-10-20 14:43:23 -0500

    Created a huffman-tree constructor

--- src/huffman.rs
+++ src/huffman.rs
@@ -1,6 +1,8 @@
 #![allow(dead_code)]
 // https://www.techiedelight.com/huffman-coding/
 use std::cmp::Ordering;
+use std::collections::BinaryHeap;
+use std::collections::HashMap;
 
 #[derive(Debug, Eq, Clone)]
 pub struct HuffmanNode {
@@ -25,5 +27,51 @@ impl PartialOrd for HuffmanNode {
 impl PartialEq for HuffmanNode {
     fn eq (&self, other: &Self) -> bool {
         return self.freq_value == other.freq_value;
+    }
+}
+
+impl HuffmanNode {
+    pub fn new(data: &str) -> HuffmanNode {
+
+        let mut freq_map = HashMap::new();
+        let mut min_heap:BinaryHeap<HuffmanNode> = BinaryHeap::new();
+
+        // create a frequency map, and build each huffman node
+        for c in data.chars() {
+            if !freq_map.contains_key(&c){
+                freq_map.insert(c, HuffmanNode {freq_value: 1, value: Some(c), left: None, right: None});
+            }
+            else{
+                let mut item = freq_map.get_mut(&c).unwrap();
+
+                item.freq_value = item.freq_value + 1;
+            }
+        }
+
+        for (_, v) in freq_map.into_iter(){
+            min_heap.push(v);
+        }
+
+        while let Some(node1) = min_heap.pop() {
+            let tmp_node2 = min_heap.pop();
+
+            if !tmp_node2.is_some(){
+                return node1;
+            }
+
+            let node2 = tmp_node2.unwrap();
+
+            let merged_node = HuffmanNode {
+                freq_value: node1.freq_value + node2.freq_value,
+                value: None,
+                left: Some(Box::new(node1)),
+                right: Some(Box::new(node2))
+            };
+
+            min_heap.push(merged_node);
+        }
+
+        // should never get down here.
+        return HuffmanNode {freq_value: 1, value: Some('d'), left: None, right: None}
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -17,55 +17,15 @@ fn main() {
 
 
 fn readFile(){
-    let mut heap = BinaryHeap::new();
 
     let path = Path::new("./resources/red.txt");
 
     let mut file = File::open(path).expect("Failed to open file");
 
-    let mut s = String::new();
-    file.read_to_string(&mut s).expect("Couldn't read file");
+    let mut red = String::new();
+    file.read_to_string(&mut red).expect("Couldn't read file");
 
-    let mut occuranses = HashMap::new();
+    let n = huffman::HuffmanNode::new("this is a huffman test");
 
-
-    for c in s.chars(){
-        if !occuranses.contains_key(&c){
-            occuranses.insert(c, huffman::HuffmanNode {freq_value: 1, value: Some(c), left: None, right: None});
-        }
-        else{
-            let mut item = occuranses.get_mut(&c).unwrap();
-
-            item.freq_value = item.freq_value + 1;
-        }
-    }
-
-    for (_, v) in occuranses.into_iter() {
-        heap.push(v);
-    }
-
-    while let Some(node) = heap.pop() {
-        let node2 = heap.pop();
-        
-        if !&node2.is_some(){
-            println!("Last node!");
-            println!("{:#?}", node);
-            continue;
-        }
-
-        let node2_unwrapped = node2.unwrap();
-
-        let mut newNode = huffman::HuffmanNode {
-            freq_value: node.freq_value +&node2_unwrapped.freq_value,
-            value: None,
-            left: Some(Box::new(node)),
-            right: Some(Box::new(node2_unwrapped))
-        };
-
-        heap.push(newNode);
-
-
-
-        //println!("{:#?} {:#?}", node.freq_value, node.value.unwrap())
-    }
+    println!("{:#?}", n)
 }
